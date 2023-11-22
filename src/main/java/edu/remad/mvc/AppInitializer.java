ba4f883c938c5a57b2812afc8ff5de48c086f098
Remy Meier <remad@web.de>
2023-11-22 09:34:51 +0100

    master: App initializer bootstraps spring web mvc. Gitignore added to ignore eclipse specific files.

---
+++ src/main/java/edu/remad/mvc/AppInitializer.java
@@ -0,0 +1,15 @@
+package edu.remad.mvc;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.WebApplicationInitializer;
+
+public class AppInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		System.out.println("Hello World");
+	}
+
+}
