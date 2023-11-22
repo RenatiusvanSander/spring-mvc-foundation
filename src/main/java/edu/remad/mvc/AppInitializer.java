d13e9f13ae0cb274d18e4cfebf69d859d69b6c31
Remy Meier <remad@web.de>
2023-11-22 12:15:55 +0100

    master: registered DispatcherServlet

--- src/main/java/edu/remad/mvc/AppInitializer.java
+++ src/main/java/edu/remad/mvc/AppInitializer.java
@@ -2,13 +2,22 @@ package edu.remad.mvc;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class AppInitializer implements WebApplicationInitializer {
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
-		System.out.println("Hello World");
+		AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
+		dispatcherContext.register(WebConfig.class);
+		
+		DispatcherServlet dispatcherServlet = new DispatcherServlet(dispatcherContext);
+		ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher", dispatcherServlet);
+		registration.addMapping("/");
+		registration.setLoadOnStartup(1); // first registered servlet in application
 	}
 }
---
+++ src/main/java/edu/remad/mvc/WebConfig.java
@@ -0,0 +1,10 @@
+package edu.remad.mvc;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@ComponentScan("edu.remad.mvc")
+public class WebConfig {
+	
+}
