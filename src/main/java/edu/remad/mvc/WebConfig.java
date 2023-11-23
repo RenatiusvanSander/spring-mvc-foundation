0284619032530cf1ea55312fd857f25ebdefe4a4
RenatiusvanSander <remad@web.de>
2023-11-23 12:37:21 +0100

    master: viewresolver for JSP has been added

--- src/main/java/edu/remad/mvc/WebConfig.java
+++ src/main/java/edu/remad/mvc/WebConfig.java
@@ -1,10 +1,20 @@
 package edu.remad.mvc;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @ComponentScan("edu.remad.mvc.controllers")
 public class WebConfig {
 	
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
 }
--- src/main/java/edu/remad/mvc/controllers/MainController.java
+++ src/main/java/edu/remad/mvc/controllers/MainController.java
@@ -2,6 +2,7 @@ package edu.remad.mvc.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,10 +14,11 @@ public class MainController {
 
 	@Autowired
 	public GreetingService greetingService;
-	
-	@ResponseBody
+
 	@RequestMapping("/")
-	public String message() {
-		return greetingService.greet();
+	public String message(Model model) {
+		model.addAttribute("message", greetingService.greet());
+		
+		return "hello";
 	}
 }
